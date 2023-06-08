7b4f619c96d0caf93bba8b6cc881f0972b189328
AchyuthSiddha <achyuthsiddha43@gmail.com>
2023-06-08 20:19:03 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 
 from rest_framework.response import Response
+
+from rest_framework.permissions import IsAuthenticated
 
 from app.seralizer import *
 
 from app.models import *
 
 @api_view()
+@permission_classes([IsAuthenticated])
 def employee(request):
     EQS=Employee.objects.all()
     EOD=EmployeeSF(EQS,many=True)
